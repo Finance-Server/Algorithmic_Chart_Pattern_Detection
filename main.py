6b8d63110686015d5ccc77c5e952956998b47fa1
MaximePro <maxime.desaniau@esme.fr>
2021-12-16 15:08:49 +0100

    add stat and plot

--- main.py
+++ main.py
@@ -1,47 +1,73 @@
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+import os.path
+from os import path
 
 from function import find_extrema, plot_window, find_patterns
 
 
 if __name__ == '__main__':
 
-    data = pd.read_csv('PFE.csv', ',')
-    #data.set_index('Date', inplace=True)
+    data = pd.read_csv('testdata.csv', ';')
+    data.set_index('Date', inplace=True)
 
     ######################################################
     # Smoothing the Noise and Find the Minama and Maxima #
     ######################################################
 
-    extrema, prices, smooth_extrema, smooth_prices = find_extrema(data['Close'], bw=[1.5])
+    name_ticker = list(data.columns)
+    stat_pattern = pd.DataFrame(columns=['Pattern', 'Count'])
+
+    for ticker in tqdm(name_ticker):
+        extrema, prices, smooth_extrema, smooth_prices = find_extrema(data[ticker], bw=[1.5])
 
     #plot_window(prices, extrema, smooth_prices, smooth_extrema, ax=None)
 
-    ##########################
-    # Pattern Identification #
-    ##########################
+        ##########################
+        # Pattern Identification #
+        ##########################
 
-    patterns = find_patterns(extrema)
+        patterns = find_patterns(extrema)
 
-    for name, pattern_periods in patterns.items():
-        print(f"{name}: {len(pattern_periods)} occurences")
+        result = pd.DataFrame(columns=['Pattern', 'Start_bar', 'End_bar'])
 
-    for name, pattern_periods in patterns.items():
+        for name, pattern_periods in patterns.items():
+            stat_pattern = stat_pattern.append({'Pattern':name, 'Count':int(len(pattern_periods))}, ignore_index=True)
 
-        print(name)
+            rows = int(np.ceil(len(pattern_periods)/2))
+            f, axes = plt.subplots(rows, 2, figsize=(20, 5*rows))
+            axes = axes.flatten()
 
-        rows = int(np.ceil(len(pattern_periods)/2))
-        f, axes = plt.subplots(rows, 2, figsize=(20, 5*rows))
-        axes = axes.flatten()
+            i = 0
+            for start, end in pattern_periods:
+                result = result.append({'Pattern':name, 'Start_bar':start, 'End_bar':end}, ignore_index=True)
+        
+                s = prices.index[start-1]
+                e = prices.index[end+6]
 
-        i = 0
-        for start, end in pattern_periods:
-            s = prices.index[start-1]
-            e = prices.index[end+1]
+                plot_window(prices[s:e], extrema.loc[s:e],
+                            smooth_prices[s:e],
+                            smooth_extrema.loc[s:e], ax=axes[i])
+                i+=1
+            
+            folder = '/Users/Maxou/Documents/Cours/PROGRAMMATION/FINANCE/Algorithmic_Chart_Pattern_Detection/Resultat/'+str(ticker)
+            if path.exists(folder) == False:
+                os.mkdir(folder)
+            
+            plt.savefig('/Users/Maxou/Documents/Cours/PROGRAMMATION/FINANCE/Algorithmic_Chart_Pattern_Detection/Resultat/'+str(ticker)+'/'+str(name)+'.png')
+            result.to_csv('/Users/Maxou/Documents/Cours/PROGRAMMATION/FINANCE/Algorithmic_Chart_Pattern_Detection/Patterns/Pattern_'+str(ticker)+'.csv')
+    
+    list_pattern = (np.unique(stat_pattern['Pattern']))
+    stat = pd.DataFrame(columns=list_pattern)
 
-            plot_window(prices[s:e], extrema.loc[s:e],
-                        smooth_prices[s:e],
-                        smooth_extrema.loc[s:e], ax=axes[i])
-            i+=1
-        plt.show()
+    for pat in list_pattern:
+        stat[pat] = stat_pattern[stat_pattern['Pattern']==pat].sum()
+    
+    stat = stat.drop(index='Pattern')
+
+    print(stat)
+    print('Done...')
+
