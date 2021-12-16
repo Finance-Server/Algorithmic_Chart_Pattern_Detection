01609c8394a39152b4fc55f13ddfcbb641aa5398
MaximePro <maxime.desaniau@esme.fr>
2021-12-16 09:06:23 +0100

    finish

--- function.py
+++ function.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import argrelextrema
 from statsmodels.nonparametric.kernel_regression import KernelReg
-
+from collections import defaultdict
 
 def find_extrema(s, bw='cv_ls'):
 
@@ -53,13 +53,90 @@ def find_extrema(s, bw='cv_ls'):
     # Return series for each with bar as index
     return extrema, prices, smooth_extrema, smooth_prices
 
-
 def plot_window(prices, extrema, smooth_prices, smooth_extrema, ax=None):
+
     if ax is None:
         fig = plt.figure()
         ax = fig.add_subplot(111)
 
     prices.plot(ax=ax, color='dodgerblue')
     ax.scatter(extrema.index, extrema.values, color='red')
-    smooth_prices.plot(ax=ax, color='lightgrey')
-    ax.scatter(smooth_extrema.index, smooth_extrema.values, color='lightgrey')
+    smooth_prices.plot(ax=ax, color='green')
+    ax.scatter(smooth_extrema.index, smooth_extrema.values, color='green')
+    #plt.show()
+
+
+def find_patterns(s, max_bars=35):
+    """
+    Input:
+        s: extrema as pd.series with bar number as index
+        max_bars: max bars for pattern to play out
+    Returns:
+        patterns: patterns as a defaultdict list of tuples
+        containing the start and end bar of the pattern
+    """
+    patterns = defaultdict(list)
+
+    # Need to start at five extrema for pattern generation
+    for i in range(5, len(s)):
+        window = s.iloc[i-5:i]
+
+        # A pattern must play out within max_bars (default 35)
+        if (window.index[-1] - window.index[0]) > max_bars:
+            continue
+
+        # Using the notation from the paper to avoid mistakes
+        e1 = window.iloc[0]
+        e2 = window.iloc[1]
+        e3 = window.iloc[2]
+        e4 = window.iloc[3]
+        e5 = window.iloc[4]
+
+        rtop_g1 = np.mean([e1,e3,e5])
+        rtop_g2 = np.mean([e2,e4])
+
+        # Head and Shoulders
+        if (e1 > e2) and (e3 > e1) and (e3 > e5) and \
+            (abs(e1 - e5) <= 0.03*np.mean([e1,e5])) and \
+            (abs(e2 - e4) <= 0.03*np.mean([e1,e5])):
+                patterns['HS'].append((window.index[0], window.index[-1]))
+
+        # Inverse Head and Shoulders
+        elif (e1 < e2) and (e3 < e1) and (e3 < e5) and \
+            (abs(e1 - e5) <= 0.03*np.mean([e1,e5])) and \
+            (abs(e2 - e4) <= 0.03*np.mean([e1,e5])):
+                patterns['IHS'].append((window.index[0], window.index[-1]))
+
+        # Broadening Top
+        elif (e1 > e2) and (e1 < e3) and (e3 < e5) and (e2 > e4):
+            patterns['BTOP'].append((window.index[0], window.index[-1]))
+
+        # Broadening Bottom
+        elif (e1 < e2) and (e1 > e3) and (e3 > e5) and (e2 < e4):
+            patterns['BBOT'].append((window.index[0], window.index[-1]))
+
+        # Triangle Top
+        elif (e1 > e2) and (e1 > e3) and (e3 > e5) and (e2 < e4):
+            patterns['TTOP'].append((window.index[0], window.index[-1]))
+
+        # Triangle Bottom
+        elif (e1 < e2) and (e1 < e3) and (e3 < e5) and (e2 > e4):
+            patterns['TBOT'].append((window.index[0], window.index[-1]))
+
+        # Rectangle Top
+        elif (e1 > e2) and (abs(e1-rtop_g1)/rtop_g1 < 0.0075) and \
+            (abs(e3-rtop_g1)/rtop_g1 < 0.0075) and (abs(e5-rtop_g1)/rtop_g1 < 0.0075) and \
+            (abs(e2-rtop_g2)/rtop_g2 < 0.0075) and (abs(e4-rtop_g2)/rtop_g2 < 0.0075) and \
+            (min(e1, e3, e5) > max(e2, e4)):
+
+            patterns['RTOP'].append((window.index[0], window.index[-1]))
+
+        # Rectangle Bottom
+        elif (e1 < e2) and (abs(e1-rtop_g1)/rtop_g1 < 0.0075) and \
+            (abs(e3-rtop_g1)/rtop_g1 < 0.0075) and (abs(e5-rtop_g1)/rtop_g1 < 0.0075) and \
+            (abs(e2-rtop_g2)/rtop_g2 < 0.0075) and (abs(e4-rtop_g2)/rtop_g2 < 0.0075) and \
+            (max(e1, e3, e5) > min(e2, e4)):
+            patterns['RBOT'].append((window.index[0], window.index[-1]))
+
+    return patterns
+
--- main.py
+++ main.py
@@ -1,6 +1,47 @@
 import pandas as pd 
 import numpy as np 
-import function
+import matplotlib.pyplot as plt
 
-data = pd.read_csv('AAPL.csv', ',')
-#data.set_index('Date', inplace=True)
+from function import find_extrema, plot_window, find_patterns
+
+
+if __name__ == '__main__':
+
+    data = pd.read_csv('AAPL.csv', ',')
+    #data.set_index('Date', inplace=True)
+
+    ######################################################
+    # Smoothing the Noise and Find the Minama and Maxima #
+    ######################################################
+
+    extrema, prices, smooth_extrema, smooth_prices = find_extrema(data['Close'], bw=[1.5])
+
+    #plot_window(prices, extrema, smooth_prices, smooth_extrema, ax=None)
+
+    ##########################
+    # Pattern Identification #
+    ##########################
+
+    patterns = find_patterns(extrema)
+
+    for name, pattern_periods in patterns.items():
+        print(f"{name}: {len(pattern_periods)} occurences")
+
+    for name, pattern_periods in patterns.items():
+
+        print(name)
+
+        rows = int(np.ceil(len(pattern_periods)/2))
+        f, axes = plt.subplots(rows, 2, figsize=(20, 5*rows))
+        axes = axes.flatten()
+
+        i = 0
+        for start, end in pattern_periods:
+            s = prices.index[start-1]
+            e = prices.index[end+1]
+
+            plot_window(prices[s:e], extrema.loc[s:e],
+                        smooth_prices[s:e],
+                        smooth_extrema.loc[s:e], ax=axes[i])
+            i+=1
+        plt.show()
